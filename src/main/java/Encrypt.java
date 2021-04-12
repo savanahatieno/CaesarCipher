c970f0ea881e38b894f1484b1619c6cc05c33931
savanahatieno <savanaht01@gmail.com>
2021-04-12 20:19:45 +0300

    encrypted the message

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -3,9 +3,15 @@ public class Encrypt {
         //start
         String text = "Hey, how's it hanging?";
 
+        System.out.println(text);
+
         char[] chars = text.toCharArray();
 
+        for(char a : chars){
+            a += 5;
+            System.out.print(a);
+        }
 
-        System.out.println(text);
+
     }
 }
