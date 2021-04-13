59d801e44337b47bd46328a7420dc2c21bb19e25
savanahatieno <savanaht01@gmail.com>
2021-04-13 15:16:08 +0300

    created a method and added length

--- src/main/java/cipher/Encrypted.java
+++ src/main/java/cipher/Encrypted.java
@@ -1,4 +1,21 @@
 package cipher;
 
 public class Encrypted {
+    public static String encrypt(String plainText, int shift) {
+        if (shift > 26) {
+            shift =  shift%26;
+        }
+        else if (shift<0){
+            shift = (shift%26)+26;
+    }
+
+        String cipherText = "";
+        //Getting length of plainText
+
+        return cipherText;
+
+   }
+    public static void main (String[]  args){
+
+    }
 }
