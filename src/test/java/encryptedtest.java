b7358fe9c6fcf1313586831946e9d4654b47c786
savanahatieno <savanaht01@gmail.com>
2021-04-15 11:15:50 +0300

    testing

--- src/test/java/encryptedtest.java
+++ src/test/java/encryptedtest.java
@@ -1,11 +1,10 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class encrypted {
-
-
-
-
-
-
+public class encryptedtest {
+    @Test
+    void shouldShowSimpleAssertion() {
+        Assertions.assertEquals(1,1);
     }
+}
 
