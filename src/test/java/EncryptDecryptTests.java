384df971b1b6930846bc314c38ba793b6da8df77
savanahatieno <savanaht01@gmail.com>
2021-04-27 12:09:02 +0300

    added some test

--- src/test/java/EncryptDecryptTests.java
+++ src/test/java/EncryptDecryptTests.java
@@ -1,11 +1,22 @@
-import org.junit.jupiter.api.Assertions;
+import cipher.Encrypted;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class EncryptDecryptTests {
+    Encrypted testEncrypted = new Encrypted("Anah", 2);
+
     @Test
-    void shouldShowSimpleAssertation() {
-        Assertions.assertEquals(1,1);
+    public void checkIfInstances() {
+        assertEquals(true , testEncrypted instanceof Encrypted);
     }
 
+    //Ecrypted test
+    @Test
+    public void isValidPlainText()
+    {
+        assertEquals(true, testEncrypted.isValidInputText());
+    }
 
 }
